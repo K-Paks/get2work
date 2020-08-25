ae02234c63d75b5aa208d0d173a6bcf39c1c8303
pozniejzmienie <pozniejzmienie@users.noreply.github.com>
2020-08-25 16:27:09 +0200

    Code is cleaner + changed text to the desired one.

--- popup.js
+++ popup.js
@@ -1,22 +1,22 @@
-findAllURL = function changeAllURL(text){
-  var current = window.location.href;
-  if(current.startsWith(text)){
-
+findAllURL = function changeAllURL(){
     document.body.style.pointerEvents = "none";
     document.body.style.overflow = "hidden";
 
     let div = document.createElement("div");
-    div.style.position = "fixed";
-    div.style.top = "50%";
-    div.style.left = "50%";
-    div.style.transform = "translate(-50%, -50%)";
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.background = "red";
+    // div.style.position = "fixed";
+    // div.style.top = "50%";
+    // div.style.left = "50%";
+    // div.style.transform = "translate(-50%, -50%)";
+    // div.style.width = "100px";
+    // div.style.height = "100px";
+    // div.style.background = "red";
     div.style.color = "white";
+    div.style.textAlign = 'center';
+    div.style.lineHeight = '100vh';
     div.zIndex = "100000";
-    div.innerHTML = "Działa";
-    div.style.fontSize = "3rem";
+    div.style.fontSize = "10rem";
+    div.style.fontWeight = "bolder";
+    div.innerHTML = "GET TO WORK";
 
     let overlay = document.createElement("div");
     overlay.id = "overlay";
@@ -29,33 +29,22 @@ findAllURL = function changeAllURL(text){
     // overlay.style.bottom =  "0";
     overlay.style.width = "100%";
     overlay.style.height = "100%";
-    overlay.style.backgroundColor = "rgba(0, 0, 0, .5)";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, .8)";
     overlay.style.pointerEvents =  "none";
     overlay.style.zIndex =  "10000";
 
     overlay.appendChild(div);
     document.body.appendChild(overlay);
-
-
-  }
 };
 
 
 function doStuff()
 {
+  doAutoplay();
 
-  let test = document.createElement("test");
-  test.innerHTML = "<iframe src=\"silence.mp3\" allow=\"autoplay\" id=\"audio\" style=\"display: none\"></iframe>";
-  document.body.appendChild(test);
+  findAllURL("bla");
 
-  let url = chrome.runtime.getURL('audio.mp3');
-  let audio = new Audio(url);
-  audio.play();
-
-  findAllURL("https://www.youtube.com/");
-  findAllURL("https://www.facebook.com/");
-
-  setTimeout(continueExecution, 2000)
+  setTimeout(continueExecution, 50000)
 }
 
 function continueExecution()
@@ -68,3 +57,16 @@ function continueExecution()
 
 doStuff();
 
+function doAutoplay() {
+  let autoplay = document.createElement("autoplay");
+  autoplay.innerHTML = "<iframe src=\"silence.mp3\" allow=\"autoplay\" id=\"audio\" style=\"display: none\"></iframe>";
+  document.body.appendChild(autoplay);
+
+  let url = chrome.runtime.getURL('audio.mp3');
+  let audio = new Audio(url);
+  audio.play();
+}
+
+// https://stackoverflow.com/questions/50490304/how-to-make-audio-autoplay-on-chrome autoplay
+
+// https://stackoverflow.com/questions/43424355/waiting-for-a-sound-to-stop-before-playing-the-next-sound play?
